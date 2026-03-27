aed8065a40d0f989c2ef1559cd6c3551d57a3283
Berat-Guclu <berat.guclu@otelfiyat.com>
2026-03-28 00:59:57 +0300

    Set Up spring boot

---
+++ backend/src/test/java/com/seniorapp/SeniorAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.seniorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
